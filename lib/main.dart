4a45f6b8bdc62fd21d41d78f034ffd46ce1a1bc2
trucluong47 <trucluong47@gmail.com>
2023-04-19 00:00:11 +0700

    modified main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,12 @@
 import 'package:ecommerce_app_flutter/constant/routes.dart';
 import 'package:ecommerce_app_flutter/constant/theme.dart';
+import 'package:ecommerce_app_flutter/models/favorite_provider.dart';
+import 'package:ecommerce_app_flutter/screen1.dart';
 import 'package:ecommerce_app_flutter/views/screens/HomeScreen/home_screen.dart';
 import 'package:ecommerce_app_flutter/views/screens/OnBoardingScreen/onboarding_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:provider/provider.dart';
 
 void main() {
   runApp(const MyApp());
@@ -18,12 +21,16 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Ecom',
-      theme: theme(),
-      debugShowCheckedModeBanner: false,
-      initialRoute: HomeScreen.routeName,
-      routes: route,
+    return ChangeNotifierProvider(
+      create: (context) => FavoriteProvider(),
+      child: MaterialApp(
+        title: 'Ecom',
+        theme: theme(),
+        debugShowCheckedModeBanner: false,
+        initialRoute: HomeScreen.routeName,
+        routes: route,
+        // home: const Screen1(),
+      ),
     );
   }
 }
